d6d9a2962b3c27973f06f67affab17867dcf4613
Pelluru32 <gowthampelluru@gmail.com>
2023-08-02 21:44:00 +0530

    first

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,4 @@
-var allowedOrigins = ['http://example1.com',
- 'http://localhost:3500/',
- 'http://localhost:3000']
+var allowedOrigins = [
+ 'https://mechanicswebsite.onrender.com']
 
 module.exports={allowedOrigins}
