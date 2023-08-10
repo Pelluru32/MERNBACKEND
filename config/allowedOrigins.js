0098de253de0250227b3496abb6a5f5db5f3757a
Pelluru32 <105586583+Pelluru32@users.noreply.github.com>
2023-08-10 12:46:16 +0530

    updatedconfig

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-var allowedOrigins = ['https://mechanicswebsite.onrender.com']
+var allowedOrigins = ['https://mechanicswebsite.onrender.com',"https://main.dn1zqd8yueonh.amplifyapp.com"]
 
 module.exports={allowedOrigins}
